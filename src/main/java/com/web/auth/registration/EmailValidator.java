99d3062b787ddcb9959842e4879f8480b801236e
Raey Fikre <raeycareer@gmail.com>
2021-05-09 16:25:22 -0500

    signUp commit

---
+++ src/main/java/com/web/auth/registration/EmailValidator.java
@@ -0,0 +1,12 @@
+package com.web.auth.registration;
+
+import org.springframework.stereotype.Service;
+
+import java.util.function.Predicate;
+@Service
+public class EmailValidator implements Predicate<String> {
+    @Override
+    public boolean test(String s) {
+        return true;
+    }
+}
--- src/main/java/com/web/auth/registration/RegistrationService.java
+++ src/main/java/com/web/auth/registration/RegistrationService.java
@@ -1,10 +1,31 @@
 package com.web.auth.registration;
 
+import com.web.auth.appuser.AppUser;
+import com.web.auth.appuser.AppUserRole;
+import com.web.auth.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+    private EmailValidator emailValidator;
+    private final AppUserService appUserService;
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if(!isValidEmail){
+          throw new IllegalStateException("Email is not Valid");
+        }
+
+        return appUserService.signUpUser(new AppUser(
+                request.getFirstName(),
+                request.getLastName(),
+                request.getEmail(),
+                request.getPassword(),
+                AppUserRole.USER
+
+
+        )
+        );
     }
 }
